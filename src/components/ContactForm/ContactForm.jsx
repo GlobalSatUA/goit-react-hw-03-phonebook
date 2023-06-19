3cfc9815cc963733393c18d3a7655804d6e1d07a
GlobalSatUA <alex.trenbach@gmail.com>
2023-06-19 14:34:28 +0300

    fix

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -46,7 +46,7 @@ class ContactForm extends Component {
           type="text"
           name="name"
           style={{ marginBottom: '10px', padding: '5px' }}
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          pattern="^[A-Za-z\u0080-\uFFFF ']+$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
           value={name}
@@ -57,7 +57,7 @@ class ContactForm extends Component {
           type="tel"
           name="number"
           style={{ marginBottom: '10px', padding: '5px' }}
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          pattern="^(\+?[0-9.\(\)\-\s]*)$"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
           value={number}
